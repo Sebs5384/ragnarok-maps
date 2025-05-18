f1fb4c32de6ed1bef62da460a6e5cf0217d87b9d
Seb <linoshizu@gmail.com>
2025-05-18 09:38:19 -0600

    Add BotSection component
    This will display a small section where we'll be let the user invite the Bot in case he doesn't want to visit the website

---
+++ components/Home/BotSection.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import Button from "../common/Button";
+import Image from "next/image";
+import styles from "./Home.module.css";
+
+function BotSection(): React.ReactElement {
+    return (
+        <section className={styles.homeSection}>
+            <h1 className={styles.homeTitles}>Lazy to leave Discord?</h1>
+            <div className={styles.botSectionContainer}>
+                <div className={styles.botDescription}>
+                    <h1><strong>Have a look at the Discord bot</strong></h1>
+                    <div className={styles.botImageWrapper}>
+                        <Image 
+                            src={"/img/count_von_count.png"}
+                            alt="bot image"
+                            fill
+                            className={styles.botImage}
+                        />
+                    </div>
+                </div>
+                <div className={styles.botSpecsSection}>
+                    <h1><strong>Features</strong></h1>
+                    <ul>
+                        <li>/locate - Search and mark maps</li>
+                        <li>/missionmonster - Get killing missions specifications by name</li>
+                        <li>/missionnpc - Find summer NPCs and their path to it by name</li>
+                        <li>All without leaving Discord</li>
+                    </ul>
+                </div>
+                <div className={styles.botInviteSection}>
+                    <Button 
+                        onClick={() => console.log("clicked")} 
+                        className={styles.inviteButton}
+                    >
+                        Get the bot
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default BotSection;
