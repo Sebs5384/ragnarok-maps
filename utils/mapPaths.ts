89675c9ccd711de60695523036628a551b23b3ec
Seb <linoshizu@gmail.com>
2025-06-05 17:31:13 -0600

    Creates mapPaths file
    This will hold all the imports for our static files in order to use them on the collage with blur as our placeholder

---
+++ utils/mapPaths.ts
@@ -0,0 +1,272 @@
+import { StaticImageData } from "next/image";
+import img0 from "@/public/maps/alb2trea.png";
+import img1 from "@/public/maps/amatsu.png";
+import img2 from "@/public/maps/ama_fild01.png";
+import img3 from "@/public/maps/anthell02.png";
+import img4 from "@/public/maps/aru_gld.png";
+import img5 from "@/public/maps/ayothaya.png";
+import img6 from "@/public/maps/ayo_dun01.png";
+import img7 from "@/public/maps/ayo_fild01.png";
+import img8 from "@/public/maps/ayo_fild02.png";
+import img9 from "@/public/maps/beach_dun.png";
+import img10 from "@/public/maps/beach_dun2.png";
+import img11 from "@/public/maps/beach_dun3.png";
+import img12 from "@/public/maps/brasilis.png";
+import img13 from "@/public/maps/bra_dun02.png";
+import img14 from "@/public/maps/bra_fild01.png";
+import img15 from "@/public/maps/cmd_fild01.png";
+import img16 from "@/public/maps/cmd_fild02.png";
+import img17 from "@/public/maps/cmd_fild04.png";
+import img18 from "@/public/maps/cmd_fild05.png";
+import img19 from "@/public/maps/cmd_fild06.png";
+import img20 from "@/public/maps/cmd_fild07.png";
+import img21 from "@/public/maps/cmd_fild08.png";
+import img22 from "@/public/maps/cmd_fild09.png";
+import img23 from "@/public/maps/dewata.png";
+import img24 from "@/public/maps/dew_dun01.png";
+import img25 from "@/public/maps/dew_fild01.png";
+import img26 from "@/public/maps/dic_dun02.png";
+import img27 from "@/public/maps/ein_dun02.png";
+import img28 from "@/public/maps/gef_dun00.png";
+import img29 from "@/public/maps/gef_dun01.png";
+import img30 from "@/public/maps/gef_dun02.png";
+import img31 from "@/public/maps/gef_fild00.png";
+import img32 from "@/public/maps/gef_fild02.png";
+import img33 from "@/public/maps/gef_fild10.png";
+import img34 from "@/public/maps/gef_fild11.png";
+import img35 from "@/public/maps/gef_fild12.png";
+import img36 from "@/public/maps/gl_cas01.png";
+import img37 from "@/public/maps/gl_chyard.png";
+import img38 from "@/public/maps/gonryun.png";
+import img39 from "@/public/maps/gon_dun02.png";
+import img40 from "@/public/maps/gon_dun03.png";
+import img41 from "@/public/maps/gon_fild01.png";
+import img42 from "@/public/maps/hu_fild01.png";
+import img43 from "@/public/maps/hu_fild02.png";
+import img44 from "@/public/maps/hu_fild06.png";
+import img45 from "@/public/maps/ice_dun01.png";
+import img46 from "@/public/maps/ice_dun02.png";
+import img47 from "@/public/maps/ice_dun03.png";
+import img48 from "@/public/maps/in_sphinx2.png";
+import img49 from "@/public/maps/izlu2dun.png";
+import img50 from "@/public/maps/iz_dun02.png";
+import img51 from "@/public/maps/iz_dun05.png";
+import img52 from "@/public/maps/jawaii.png";
+import img53 from "@/public/maps/kh_dun02.png";
+import img54 from "@/public/maps/lhz_fild01.png";
+import img55 from "@/public/maps/lhz_fild02.png";
+import img56 from "@/public/maps/lighthalzen.png";
+import img57 from "@/public/maps/louyang.png";
+import img58 from "@/public/maps/lou_dun01.png";
+import img59 from "@/public/maps/lou_dun02.png";
+import img60 from "@/public/maps/lou_fild01.png";
+import img61 from "@/public/maps/malangdo.png";
+import img62 from "@/public/maps/malaya.png";
+import img63 from "@/public/maps/mal_dun01.png";
+import img64 from "@/public/maps/man_fild03.png";
+import img65 from "@/public/maps/ma_fild01.png";
+import img66 from "@/public/maps/mjolnir_02.png";
+import img67 from "@/public/maps/mjolnir_04.png";
+import img68 from "@/public/maps/mjolnir_05.png";
+import img69 from "@/public/maps/mjolnir_07.png";
+import img70 from "@/public/maps/mjolnir_08.png";
+import img71 from "@/public/maps/mjolnir_09.png";
+import img72 from "@/public/maps/mjolnir_10.png";
+import img73 from "@/public/maps/mjo_dun01.png";
+import img74 from "@/public/maps/moc_fild01.png";
+import img75 from "@/public/maps/moc_fild02.png";
+import img76 from "@/public/maps/moc_fild03.png";
+import img77 from "@/public/maps/moc_fild07.png";
+import img78 from "@/public/maps/moc_fild11.png";
+import img79 from "@/public/maps/moc_fild12.png";
+import img80 from "@/public/maps/moc_fild13.png";
+import img81 from "@/public/maps/moc_fild16.png";
+import img82 from "@/public/maps/moc_fild17.png";
+import img83 from "@/public/maps/moc_fild18.png";
+import img84 from "@/public/maps/moc_pryd02.png";
+import img85 from "@/public/maps/moc_ruins.png";
+import img86 from "@/public/maps/moscovia.png";
+import img87 from "@/public/maps/nif_fild01.png";
+import img88 from "@/public/maps/orcsdun01.png";
+import img89 from "@/public/maps/pay_dun01.png";
+import img90 from "@/public/maps/pay_dun03.png";
+import img91 from "@/public/maps/pay_dun04.png";
+import img92 from "@/public/maps/pay_fild04.png";
+import img93 from "@/public/maps/pay_fild06.png";
+import img94 from "@/public/maps/pay_fild07.png";
+import img95 from "@/public/maps/pay_fild09.png";
+import img96 from "@/public/maps/pay_fild10.png";
+import img97 from "@/public/maps/pay_fild11.png";
+import img98 from "@/public/maps/prt_fild02.png";
+import img99 from "@/public/maps/prt_fild06.png";
+import img100 from "@/public/maps/prt_fild09.png";
+import img101 from "@/public/maps/prt_fild10.png";
+import img102 from "@/public/maps/prt_fild11.png";
+import img103 from "@/public/maps/prt_maze01.png";
+import img104 from "@/public/maps/prt_sewb1.png";
+import img105 from "@/public/maps/ra_fild01.png";
+import img106 from "@/public/maps/ra_fild02.png";
+import img107 from "@/public/maps/ra_fild05.png";
+import img108 from "@/public/maps/ra_fild06.png";
+import img109 from "@/public/maps/ra_fild07.png";
+import img110 from "@/public/maps/ra_fild08.png";
+import img111 from "@/public/maps/ra_fild10.png";
+import img112 from "@/public/maps/ra_fild12.png";
+import img113 from "@/public/maps/ra_fild13.png";
+import img114 from "@/public/maps/ra_san05.png";
+import img115 from "@/public/maps/spl_fild03.png";
+import img116 from "@/public/maps/treasure02.png";
+import img117 from "@/public/maps/tur_dun02.png";
+import img118 from "@/public/maps/tur_dun04.png";
+import img119 from "@/public/maps/um_dun02.png";
+import img120 from "@/public/maps/um_fild01.png";
+import img121 from "@/public/maps/um_fild02.png";
+import img122 from "@/public/maps/um_fild03.png";
+import img123 from "@/public/maps/um_fild04.png";
+import img124 from "@/public/maps/ve_fild01.png";
+import img125 from "@/public/maps/ve_fild03.png";
+import img126 from "@/public/maps/ve_fild04.png";
+import img127 from "@/public/maps/ve_fild05.png";
+import img128 from "@/public/maps/ve_fild06.png";
+import img129 from "@/public/maps/ve_fild07.png";
+import img130 from "@/public/maps/xmas_fild01.png";
+import img131 from "@/public/maps/yuno_fild03.png";
+import img132 from "@/public/maps/yuno_fild12.png";
+
+const mapImages: Record<string, StaticImageData> = {
+  "alb2trea": img0,
+  "amatsu": img1,
+  "ama_fild01": img2,
+  "anthell02": img3,
+  "aru_gld": img4,
+  "ayothaya": img5,
+  "ayo_dun01": img6,
+  "ayo_fild01": img7,
+  "ayo_fild02": img8,
+  "beach_dun": img9,
+  "beach_dun2": img10,
+  "beach_dun3": img11,
+  "brasilis": img12,
+  "bra_dun02": img13,
+  "bra_fild01": img14,
+  "cmd_fild01": img15,
+  "cmd_fild02": img16,
+  "cmd_fild04": img17,
+  "cmd_fild05": img18,
+  "cmd_fild06": img19,
+  "cmd_fild07": img20,
+  "cmd_fild08": img21,
+  "cmd_fild09": img22,
+  "dewata": img23,
+  "dew_dun01": img24,
+  "dew_fild01": img25,
+  "dic_dun02": img26,
+  "ein_dun02": img27,
+  "gef_dun00": img28,
+  "gef_dun01": img29,
+  "gef_dun02": img30,
+  "gef_fild00": img31,
+  "gef_fild02": img32,
+  "gef_fild10": img33,
+  "gef_fild11": img34,
+  "gef_fild12": img35,
+  "gl_cas01": img36,
+  "gl_chyard": img37,
+  "gonryun": img38,
+  "gon_dun02": img39,
+  "gon_dun03": img40,
+  "gon_fild01": img41,
+  "hu_fild01": img42,
+  "hu_fild02": img43,
+  "hu_fild06": img44,
+  "ice_dun01": img45,
+  "ice_dun02": img46,
+  "ice_dun03": img47,
+  "in_sphinx2": img48,
+  "izlu2dun": img49,
+  "iz_dun02": img50,
+  "iz_dun05": img51,
+  "jawaii": img52,
+  "kh_dun02": img53,
+  "lhz_fild01": img54,
+  "lhz_fild02": img55,
+  "lighthalzen": img56,
+  "louyang": img57,
+  "lou_dun01": img58,
+  "lou_dun02": img59,
+  "lou_fild01": img60,
+  "malangdo": img61,
+  "malaya": img62,
+  "mal_dun01": img63,
+  "man_fild03": img64,
+  "ma_fild01": img65,
+  "mjolnir_02": img66,
+  "mjolnir_04": img67,
+  "mjolnir_05": img68,
+  "mjolnir_07": img69,
+  "mjolnir_08": img70,
+  "mjolnir_09": img71,
+  "mjolnir_10": img72,
+  "mjo_dun01": img73,
+  "moc_fild01": img74,
+  "moc_fild02": img75,
+  "moc_fild03": img76,
+  "moc_fild07": img77,
+  "moc_fild11": img78,
+  "moc_fild12": img79,
+  "moc_fild13": img80,
+  "moc_fild16": img81,
+  "moc_fild17": img82,
+  "moc_fild18": img83,
+  "moc_pryd02": img84,
+  "moc_ruins": img85,
+  "moscovia": img86,
+  "nif_fild01": img87,
+  "orcsdun01": img88,
+  "pay_dun01": img89,
+  "pay_dun03": img90,
+  "pay_dun04": img91,
+  "pay_fild04": img92,
+  "pay_fild06": img93,
+  "pay_fild07": img94,
+  "pay_fild09": img95,
+  "pay_fild10": img96,
+  "pay_fild11": img97,
+  "prt_fild02": img98,
+  "prt_fild06": img99,
+  "prt_fild09": img100,
+  "prt_fild10": img101,
+  "prt_fild11": img102,
+  "prt_maze01": img103,
+  "prt_sewb1": img104,
+  "ra_fild01": img105,
+  "ra_fild02": img106,
+  "ra_fild05": img107,
+  "ra_fild06": img108,
+  "ra_fild07": img109,
+  "ra_fild08": img110,
+  "ra_fild10": img111,
+  "ra_fild12": img112,
+  "ra_fild13": img113,
+  "ra_san05": img114,
+  "spl_fild03": img115,
+  "treasure02": img116,
+  "tur_dun02": img117,
+  "tur_dun04": img118,
+  "um_dun02": img119,
+  "um_fild01": img120,
+  "um_fild02": img121,
+  "um_fild03": img122,
+  "um_fild04": img123,
+  "ve_fild01": img124,
+  "ve_fild03": img125,
+  "ve_fild04": img126,
+  "ve_fild05": img127,
+  "ve_fild06": img128,
+  "ve_fild07": img129,
+  "xmas_fild01": img130,
+  "yuno_fild03": img131,
+  "yuno_fild12": img132,
+};
+
+export default mapImages;
