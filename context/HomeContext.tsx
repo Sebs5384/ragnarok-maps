c6c76b4d03d0250d53a05616c332005a5dce5c37
Seb <linoshizu@gmail.com>
2025-06-09 14:52:18 -0600

    Defines context props
    This will fix a TS error where the values of the context weren't specified thus giving us wrong usage of types

--- context/HomeContext.tsx
+++ context/HomeContext.tsx
@@ -2,24 +2,30 @@
 
 import { useContext, createContext } from "react";
 import { useGetShuffledItems, useFetchMaps } from "../hooks/index";
+import { IHomeContextProps, IChildren } from "./interfaces/index";
 
-interface Children {
-    children: React.ReactNode;
-}
+export const HomeContext = createContext<IHomeContextProps | undefined>(undefined);
 
-export const HomeContext = createContext({});
-
-export function HomeProvider({ children }: Children): React.ReactElement {
+export function HomeProvider({ children }: IChildren): React.ReactElement {
     const MAX_MAPS = 11;
 
     const { loading, maps, error } = useFetchMaps();
     const { items, hasShuffle } = useGetShuffledItems(maps, MAX_MAPS);
 
+
     return (
         <HomeContext.Provider value={{ items, hasShuffle }}>
             {children}
         </HomeContext.Provider>
-    )
+    );
 };
 
-export const useHomeContext = () => useContext(HomeContext);
+export const useHomeContext = (): IHomeContextProps => {
+    const context = useContext(HomeContext);
+    
+    if(!context) {
+        throw new Error("useHomeContext must be used within a HomeProvider");
+    };
+
+    return context;
+};
