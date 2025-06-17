4b351ad42ad6ccf2bfce4e6038d8319e0baf63a6
Seb <linoshizu@gmail.com>
2025-06-17 16:14:35 -0600

    Updates Navbar
    This will integrate the new components to Navbar as well as give it a conditional style when the route is selected

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,16 +1,40 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import React from "react";
 import Link from "next/link";
 import Bar from "./Bar";
 import Logo from "./Logo";
+import Searchbox from "./Searchbox";
+import Magnifier from "./Magnifier";
 import styles from "./Navbar.module.css"
+import clsx from "clsx";
 
 function Navbar(): React.ReactElement {
+    const pathname = usePathname();
+    const showSearchbox = pathname === "/maps";
+
     return (
         <Bar className={styles.navbar}>
-            <Logo className={styles.logo}/>
-            <Link className={styles.navItem} href="/maps">Maps</Link>
+            <div className={styles.leftSection}>
+                <Logo className={styles.logo}/>
+                <Link className={clsx(
+                    styles.navItem,
+                    {
+                        [styles.navItemActive]: pathname === "/maps"
+                    }
+                )} 
+                    href="/maps?limit=40&offset=0">Maps
+                </Link>
+            </div>
+            {
+                showSearchbox && (
+                    <div className={styles.centerSection}>
+                        <Magnifier />
+                        <Searchbox className={styles.searchbox}/>
+                    </div>
+                )   
+            }
         </Bar>
     );
 };
