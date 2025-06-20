ba01b92fa70bbaf77c0a9e3c783cc9d67bcd9915
Seb <linoshizu@gmail.com>
2025-06-20 17:07:59 -0600

    Update App file paths
    We'll be using the @ to go directly to root and then path it through there

--- app/api/maps/route.ts
+++ app/api/maps/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { readJsonFile } from "../../../utils/general";
+import { readJsonFile } from "@/utils/general";
 
 
 export async function GET(req: Request) {
