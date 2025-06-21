e5b3918617c2aeefdb2c09f9a97fd4af602fa77a
Seb <linoshizu@gmail.com>
2025-06-21 17:46:53 -0600

    Creates useHandlePagination
    This hook will be in charge of dispatching the paginator states as well as its logic

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,5 @@
 import useGetShuffledItems from "@/hooks/useGetShuffledItems";
 import useFetchMaps from "@/hooks/useFetchMaps";
+import useHandlePagination from "@/hooks/useHandlePagination";
 
-export { useGetShuffledItems, useFetchMaps };
+export { useGetShuffledItems, useFetchMaps, useHandlePagination };
--- hooks/useHandlePagination.ts
+++ hooks/useHandlePagination.ts
@@ -1,2 +1,36 @@
 import { useEffect, useReducer } from "react";
 import { paginatorReducer, initialPaginatorState } from "@/reducers/index";
+import { IMapsData, IPaginatorState } from "@/reducers/interfaces/index";
+
+function useHandlePagination({ limit, offset, items}: { limit: number, offset: number, items: IMapsData[] }): IPaginatorState {
+    const [state, dispatch] = useReducer(paginatorReducer, initialPaginatorState);
+
+    useEffect(() => {
+        if(!items || items.length === 0) return;
+
+        try {
+            const totalPages = Array.from({ length: Math.ceil(items.length / limit) }, (_, index) => index + 1);
+            const lastPage = Math.floor(items.length / limit) * limit;
+            const currentPage = Math.floor(offset / limit) + 1;
+            const mapsInPage = items.slice(offset, offset + limit);
+            
+            if(totalPages.length) {
+                dispatch({ type: "SET_TOTAL_PAGES", payload: totalPages });
+                dispatch({ type: "SET_LAST_PAGE", payload: lastPage });
+                dispatch({ type: "SET_CURRENT_PAGE", payload: currentPage });
+                dispatch({ type: "SET_MAPS_IN_PAGE", payload: mapsInPage });
+            };
+        } catch (error) {
+            console.error(error);
+        };
+    }, [items, limit, offset]);
+
+    return {
+        totalPages: state.totalPages,
+        lastPage: state.lastPage,
+        currentPage: state.currentPage,
+        mapsInPage: state.mapsInPage
+    };
+};
+
+export default useHandlePagination;
