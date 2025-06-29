a594b98e7623d76cdf53f85bc336ba50d3700d75
Seb <linoshizu@gmail.com>
2025-06-28 18:14:05 -0600

    Changes maps page to client

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import { MapsProvider } from "@/context/MapsContext";
 import Grid from "@/components/Maps/Grid";
 import Paginator from "@/components/Paginator/Paginator";
 import styles from "@/components/Maps/Maps.module.css";
 
-function Maps({ searchParams }: { searchParams: { [key: string]: string | undefined } }): React.ReactElement {
-    const limit = searchParams.limit ? parseInt(searchParams.limit) : 40;
-    const offset = searchParams.offset ? parseInt(searchParams.offset) : 0;
+function Maps(): React.ReactElement {
+    const searchParams = useSearchParams();
+    const limit = parseInt(searchParams.get("limit") || "40");
+    const offset = parseInt(searchParams.get("offset") || "0");
+    const search = searchParams.get("search") || "";
 
     return (
-        <MapsProvider limit={limit} offset={offset} >
+        <MapsProvider search={search} limit={limit} offset={offset} >
             <main className={styles.home}>
                 <Grid />
                 <Paginator />
