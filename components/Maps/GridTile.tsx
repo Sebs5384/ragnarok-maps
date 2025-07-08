59b32e5843450877cdd49b6f1507e07b4ce0aebd
Seb <linoshizu@gmail.com>
2025-07-08 17:08:37 -0600

    Add slug parameter on GridTile component

--- components/Maps/GridTile.tsx
+++ components/Maps/GridTile.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import mapImages from "@/utils/mapPaths";
 import styles from "./Maps.module.css";
 
-function GridTile({id, name}: {id: number, name: string}): React.ReactElement {
+function GridTile({id, name, slug}: {id: number, name: string, slug: string}): React.ReactElement {
     return (
         <div className={styles.tile}>
             <div className={styles.tileTextContainer}>
                 <strong className={styles.tileText}>#{id} - {name}</strong>
             </div>
             <div className={styles.tileContainer}>
-                <Image 
-                    src={mapImages[`${name}`]}
-                    alt={`map-${id}`}
-                    fill
-                    placeholder="blur"
-                />
+                <Link href={`http://localhost:3000/maps/${slug}`}>
+                    <Image 
+                        src={mapImages[`${name}`]}
+                        alt={`map-${id}`}
+                        fill
+                        placeholder="blur"
+                    />
+                </Link>
             </div>
         </div>
     );
