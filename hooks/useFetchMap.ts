c25df4edd4802c7cb9028bf454e4d0119a60446c
Seb <linoshizu@gmail.com>
2025-07-13 12:45:04 -0600

    Creates useFetchMap
    This hook will be responsible of the states of the selected map from the grid

---
+++ hooks/useFetchMap.ts
@@ -0,0 +1,29 @@
+import { useEffect, useReducer } from "react";
+import { mapReducer, initialMapState } from "@/reducers/index";
+import { IMapState } from "@/reducers/interfaces/index";
+import { getMap } from "@/api/map";
+
+function useFetchMaps(id?: number, name?: string, slug?: string): IMapState {
+    const [state, dispatch] = useReducer(mapReducer, initialMapState);
+
+    useEffect(() => {
+        const fetchMap = async () => {
+            dispatch({ type: "FETCH_REQUEST", payload: null });
+
+            try {
+                const map = await getMap(id, name, slug);
+                dispatch({ type: "FETCH_SUCCESS", payload: map });
+            } catch(error) {
+                dispatch({ type: "FETCH_FAILURE", payload: error });
+            };
+        };
+
+        fetchMap();
+    }, [id, name, slug]);
+
+    return {
+        loading: state.loading,
+        map: state.map,
+        error: state.error
+    };
+};
