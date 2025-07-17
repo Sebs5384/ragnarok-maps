71d7bd62f5321088339cac9203d26ca734165451
Seb <linoshizu@gmail.com>
2025-07-17 17:57:42 -0600

    Update hook index

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,6 +1,13 @@
 import useGetShuffledItems from "@/hooks/useGetShuffledItems";
+import useFetchMap from "@/hooks/useFetchMap";
 import useFetchMaps from "@/hooks/useFetchMaps";
 import useHandlePagination from "@/hooks/useHandlePagination";
 import useHandleSearchbox from "@/hooks/useHandleSearchbox";
 
-export { useGetShuffledItems, useFetchMaps, useHandlePagination, useHandleSearchbox };
+export { 
+    useGetShuffledItems, 
+    useFetchMap,
+    useFetchMaps, 
+    useHandlePagination, 
+    useHandleSearchbox 
+};
