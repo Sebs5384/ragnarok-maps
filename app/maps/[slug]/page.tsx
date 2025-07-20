da85004254b1594316338588011c01d5f4fb7dd6
Seb <linoshizu@gmail.com>
2025-07-20 16:46:21 -0600

    Creates slug page
    This component will be in charge of displaying the map information and will serve to allow the user to mark down the map through its inputs

---
+++ app/maps/[slug]/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { use } from "react";
+import { useFetchMap } from "@/hooks/index";
+import Image from "next/image";
+import styles from "@/components/Maps/Maps.module.css";
+
+function MapDetails({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = use(params);
+    const { loading, map, error } = useFetchMap(undefined, undefined, slug);
+
+    return (
+        <>
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : map ? (
+                <section>
+                    <div className={styles.mapSection}>
+                        <div className={styles.mapImageContainer}>
+                            <Image src={`/maps/${map.name}.png`} alt={`${map.name}` } fill></Image>
+                        </div>
+                        <div className={styles.mapInformationSection}> 
+                            <div className={styles.mapInformationContainer}>
+                                <div className={styles.infoContainer}>
+                                    <div className={styles.mapIconContainer}>
+                                        <Image className={styles.mapInfoIcon} src={`/img/midgard_map.png`} alt={`${map.name}`} fill></Image>
+                                    </div>
+                                    <strong>Map name: {map.name}</strong>
+                                </div>
+                                <div className={styles.infoContainer}>
+                                    <div className={styles.mapIconContainer}>
+                                        <Image className={styles.mapInfoIcon} src={`/img/cell_icon.png`} alt={`${map.name}`} fill></Image>
+                                    </div>
+                                    <strong>Total cells: {map.cells}</strong>
+                                </div>
+                            </div>
+                            <div className={styles.mapInformationContainer}>
+                                <div className={styles.infoContainer}>
+                                    <div className={styles.mapIconContainer}>
+                                        <Image className={styles.mapInfoIcon} src={`/img/width_icon.png`} alt={`${map.name}`} fill></Image>
+                                    </div>
+                                    <strong>Map Width: {map.width}px</strong>
+                                </div>
+                                <div className={styles.lastInfoContainer}>
+                                    <div className={styles.mapIconContainer}>
+                                        <Image className={styles.mapInfoIcon} src={`/img/height_icon.png`} alt={`${map.name}`} fill></Image>
+                                    </div>
+                                    <strong>Map Height: {map.height}px</strong>
+                                </div>
+                            </div>
+                        </div>     
+                    </div>       
+                </section>
+            ) : (
+                <p>Map not found</p>
+            )}
+        </>
+    );
+};
+
+export default MapDetails;
