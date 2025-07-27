6b51836a4c7ba86933ffafaa3cebc017486cd7a3
Seb <linoshizu@gmail.com>
2025-07-26 18:36:07 -0600

    Creates Coods section component

---
+++ app/maps/[slug]/CoordinatesSection.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import Image from "next/image";
+import { IMapData } from "@/reducers/interfaces/index";
+
+function CoordinatesSeciton({ map, styles }: { map: IMapData, styles: any }): React.ReactElement {
+    return (
+        <section>
+            <div className={styles.mapSection}>
+                <div className={styles.mapImageContainer}>
+                    <Image src={`/maps/${map.name}.png`} alt={`${map.name}` } fill></Image>
+                </div>
+                <div className={styles.mapInformationSection}> 
+                    <div className={styles.mapInformationContainer}>
+                        <div className={styles.infoContainer}>
+                            <div className={styles.mapIconContainer}>
+                                <Image className={styles.mapInfoIcon} src={`/img/midgard_map.png`} alt={`${map.name}`} fill></Image>
+                            </div>
+                            <strong>Name: {map.name}</strong>
+                        </div>
+                        <div className={styles.infoContainer}>
+                            <div className={styles.mapIconContainer}>
+                                <Image className={styles.mapInfoIcon} src={`/img/cell_icon.png`} alt={`${map.name}`} fill></Image>
+                            </div>
+                            <strong>Total cells: {map.cells}</strong>
+                        </div>
+                    </div>
+                    <div className={styles.mapInformationContainer}>
+                        <div className={styles.infoContainer}>
+                            <div className={styles.mapIconContainer}>
+                                <Image className={styles.mapInfoIcon} src={`/img/width_icon.png`} alt={`${map.name}`} fill></Image>
+                            </div>
+                            <strong>Map Width: {map.width}px</strong>
+                        </div>
+                        <div className={styles.infoContainer}>
+                            <div className={styles.mapIconContainer}>
+                                <Image className={styles.mapInfoIcon} src={`/img/height_icon.png`} alt={`${map.name}`} fill></Image>
+                            </div>
+                            <strong>Map Height: {map.height}px</strong>
+                        </div>
+                    </div>
+                    <div className={styles.cordsSection}>
+                        <div>
+                            <div className={styles.inputSection}>
+                                <div>
+                                    <strong>Cords X</strong>
+                                    <input className={styles.mapCordsInput} type="number" placeholder="X" />
+                                </div>
+                                <div>
+                                    <strong>Cords Y</strong>
+                                    <input className={styles.mapCordsInput} type="number" placeholder="Y" />
+                                </div>
+                            </div>
+                            <div className={styles.cordsButtonSection}>
+                                <button className={styles.cordsButton}>Locate</button>
+                                <button className={styles.cordsButton}>Clear</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.urlSection}>
+                        <div>
+                            <div className={styles.inputSection}>
+                                <div>
+                                    <strong>URL</strong>
+                                    <input className={styles.urlCordsInput} type="text" placeholder="URL" />
+                                </div>
+                            </div>
+                            <div className={styles.cordsButtonSection}>
+                                <button className={styles.cordsButton}>Copy</button>
+                                <button className={styles.cordsButton}>Clear</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.shareSection}>
+                        <div>Share on Discord</div>
+                        <div className={styles.discordIconContainer}>
+                            <Image className={styles.discordIcon} src={"/img/discord_icon.png"} alt="discord" fill />
+                        </div>
+                    </div>
+                </div>     
+            </div>       
+        </section>
+    )
+};
+
+export default CoordinatesSeciton;
--- app/maps/[slug]/page.tsx
+++ app/maps/[slug]/page.tsx
@@ -2,7 +2,7 @@
 
 import { use } from "react";
 import { useFetchMap } from "@/hooks/index";
-import Image from "next/image";
+import CoordinatesSeciton from "./CoordinatesSection";
 import styles from "@/components/Maps/Maps.module.css";
 
 function MapDetails({ params }: { params: Promise<{ slug: string }> }) {
@@ -16,81 +16,7 @@ function MapDetails({ params }: { params: Promise<{ slug: string }> }) {
             ) : error ? (
                 <p>{error}</p>
             ) : map ? (
-                <section>
-                    <div className={styles.mapSection}>
-                        <div className={styles.mapImageContainer}>
-                            <Image src={`/maps/${map.name}.png`} alt={`${map.name}` } fill></Image>
-                        </div>
-                        <div className={styles.mapInformationSection}> 
-                            <div className={styles.mapInformationContainer}>
-                                <div className={styles.infoContainer}>
-                                    <div className={styles.mapIconContainer}>
-                                        <Image className={styles.mapInfoIcon} src={`/img/midgard_map.png`} alt={`${map.name}`} fill></Image>
-                                    </div>
-                                    <strong>Name: {map.name}</strong>
-                                </div>
-                                <div className={styles.infoContainer}>
-                                    <div className={styles.mapIconContainer}>
-                                        <Image className={styles.mapInfoIcon} src={`/img/cell_icon.png`} alt={`${map.name}`} fill></Image>
-                                    </div>
-                                    <strong>Total cells: {map.cells}</strong>
-                                </div>
-                            </div>
-                            <div className={styles.mapInformationContainer}>
-                                <div className={styles.infoContainer}>
-                                    <div className={styles.mapIconContainer}>
-                                        <Image className={styles.mapInfoIcon} src={`/img/width_icon.png`} alt={`${map.name}`} fill></Image>
-                                    </div>
-                                    <strong>Map Width: {map.width}px</strong>
-                                </div>
-                                <div className={styles.infoContainer}>
-                                    <div className={styles.mapIconContainer}>
-                                        <Image className={styles.mapInfoIcon} src={`/img/height_icon.png`} alt={`${map.name}`} fill></Image>
-                                    </div>
-                                    <strong>Map Height: {map.height}px</strong>
-                                </div>
-                            </div>
-                            <div className={styles.cordsSection}>
-                                <div>
-                                    <div className={styles.inputSection}>
-                                        <div>
-                                            <strong>Cords X</strong>
-                                            <input className={styles.mapCordsInput} type="number" placeholder="X" />
-                                        </div>
-                                        <div>
-                                            <strong>Cords Y</strong>
-                                            <input className={styles.mapCordsInput} type="number" placeholder="Y" />
-                                        </div>
-                                    </div>
-                                    <div className={styles.cordsButtonSection}>
-                                        <button className={styles.cordsButton}>Locate</button>
-                                        <button className={styles.cordsButton}>Clear</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={styles.urlSection}>
-                                <div>
-                                    <div className={styles.inputSection}>
-                                        <div>
-                                            <strong>URL</strong>
-                                            <input className={styles.urlCordsInput} type="text" placeholder="URL" />
-                                        </div>
-                                    </div>
-                                    <div className={styles.cordsButtonSection}>
-                                        <button className={styles.cordsButton}>Copy</button>
-                                        <button className={styles.cordsButton}>Clear</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={styles.shareSection}>
-                                <div>Share on Discord</div>
-                                <div className={styles.discordIconContainer}>
-                                    <Image className={styles.discordIcon} src={"/img/discord_icon.png"} alt="discord" fill />
-                                </div>
-                            </div>
-                        </div>     
-                    </div>       
-                </section>
+                <CoordinatesSeciton map={map} styles={styles} />
             ) : (
                 <p>Map not found</p>
             )}
