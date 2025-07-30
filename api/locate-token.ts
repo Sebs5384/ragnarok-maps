c44f26ef7c6469f8d498edbc312ddb67ba6dd116
Seb <linoshizu@gmail.com>
2025-07-30 13:03:24 -0600

    Creates locate-token fetch function

---
+++ api/locate-token.ts
@@ -0,0 +1,28 @@
+const BASE_URL = "http://localhost:3000";
+
+async function getLocationToken(map: string, x: string, y: string): Promise<any> {
+    const locateUrl = `${BASE_URL}/api/locate-token`;
+
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "key": process.env.LOCATE_KEY as string
+    });
+
+    const response = await fetch(locateUrl, {
+        method: "POST",
+        headers,
+        body: JSON.stringify({ map, x, y })
+    });
+
+    if(!response.ok) {
+        const error = await response.text();
+        throw new Error(error);
+    }
+
+    const token = await response.json();
+    return token;
+};
+
+export {
+    getLocationToken
+};
