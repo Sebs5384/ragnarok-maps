59e5f31468ab61310e3cdb02d454a4dcfc400e7c
Seb <linoshizu@gmail.com>
2025-08-01 06:58:33 -0600

    Creates locate-token
    This route will be in charge of generating the token that is given to locate route and requested by locate-token fetch function

---
+++ app/api/locate-token/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.JWT_SECRET;
+
+export async function POST(req: Request) {  
+    try {
+        const key = req.headers.get("key")
+        if(key !== process.env.LOCATE_KEY) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        };
+
+        if (!SECRET) {
+            console.error("JWT_SECRET is missing!");
+            return NextResponse.json({ error: "Server misconfiguration" }, { status: 500 });
+        };
+
+        const body = await req.json();
+        const { map, x, y } = body;
+
+        if (
+            typeof map !== "string" ||
+            typeof x !== "string" ||
+            typeof y !== "string"
+        ) {
+            return NextResponse.json(
+                { error: "Invalid payload" },
+                { status: 400 }
+            );
+        };
+
+        const payload = { map, x, y };
+        const token = jwt.sign(payload, SECRET ?? "", { expiresIn: "10m" });
+
+        return NextResponse.json({ token });
+    } catch (error) {
+        console.error("Token generation error:", error);
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    };
+};
