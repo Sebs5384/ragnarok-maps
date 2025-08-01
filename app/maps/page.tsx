db12176ece89210bb5ebf2cbfb83b2888e124f70
Seb <linoshizu@gmail.com>
2025-08-01 13:33:12 -0600

    Update maps page

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,8 +1,20 @@
-import MapsClient from "./MapsClient";
+import { MapsProvider } from "@/context/MapsContext";
+import Grid from "@/components/Maps/Grid";
+import Paginator from "@/components/Paginator/Paginator";
+import styles from "@/components/Maps/Maps.module.css";
 
-function Maps(): React.ReactElement {
+function Maps({ searchParams }: { searchParams: { limit: string; offset: string; search: string; }}): React.ReactElement {
+    const limit = parseInt(searchParams.limit ?? "40");
+    const offset = parseInt(searchParams.offset ?? "0");
+    const search = searchParams.search ?? "";
+
     return (
-        <MapsClient />
+        <MapsProvider search={search} limit={limit} offset={offset} >
+            <main className={styles.home}>
+                <Grid />
+                <Paginator />
+            </main>
+        </MapsProvider>
     );
 };
 
