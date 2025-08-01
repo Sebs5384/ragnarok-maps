95df6c9515c8cf70eeafef420182d38add4d3122
Seb <linoshizu@gmail.com>
2025-08-01 13:43:21 -0600

    Update maps params

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -3,7 +3,7 @@ import Grid from "@/components/Maps/Grid";
 import Paginator from "@/components/Paginator/Paginator";
 import styles from "@/components/Maps/Maps.module.css";
 
-function Maps({ searchParams }: { searchParams: { limit: string; offset: string; search: string; }}): React.ReactElement {
+function Maps({ searchParams }: { searchParams: { [key: string]: string | undefined } }): React.ReactElement {
     const limit = parseInt(searchParams.limit ?? "40");
     const offset = parseInt(searchParams.offset ?? "0");
     const search = searchParams.search ?? "";
